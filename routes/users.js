2e9f29a81275f899648c1a6e01eacb585f1fd381
home0712 <k07m12j@gmail.com>
2023-06-27 15:50:25 +0900

    [CHORE] 로그인 API

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,20 @@
 import express from 'express';
 const router = express.Router();
 
-// import { User } from '../models/index.js';
-// import loginUser from '../controller/users/loginUser.js';
-
 router.get('/login', (req, res) => {
   res.render('login', {}); // views 폴더 밑에 있는 파일을 참조함
 });
+
+router.get('/auth', (req, res) => {
+  res.render('auth', {});
+});
+
+router.get('/logout', (req, res) => {
+  res.render('logout', {});
+});
+
+// router.get('/', (req, res) => {
+//   // res.render('', {}); // views 폴더 밑에 있는 파일을 참조함
+// });
 
 export default router;
