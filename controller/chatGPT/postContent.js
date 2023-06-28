6ce2f9c3387598a88dea3123c5e675d6727cbd22
eunju <sherlock22124@gmail.com>
2023-06-28 13:55:22 +0900

    [CHORE] 대화목록 및 질문 개수 제한 수정

--- controller/chatGPT/postContent.js
+++ controller/chatGPT/postContent.js
@@ -6,67 +6,59 @@ import callChatGPT from '../../middleware/chatgpt.js';
 const postContent = async (req, res, next) => {
   try {
     const listId = Number(req.params.list_id);
-    const { company, job, prompt, type, count } = req.body;
+    const { prompt, type, count } = req.body;
     let content;
-    if (prompt) {
-      content =
-        '다음은 ' +
-        `${company}` +
-        ' ' +
-        `${job}` +
-        ' 지원 자소서야: ' +
-        `${prompt}` +
-        ' 이걸 보고 ' +
-        `${type}` +
-        '면접 질문 ' +
-        `${count}` +
-        '개 해 줘';
+    if (count >= 10) {
+      res.status(400).send('질문의 개수가 너무 많습니다.');
     } else {
-      content =
-        `${company}` +
-        ' ' +
-        `${job}` +
-        ' ' +
-        `${type}` +
-        '면접 질문 ' +
-        `${count}` +
-        '개 해 줘';
-    }
-    console.log('content = ', content);
-    // 기존 메시지가 존재하지 않는다고 가정
-    await models.ChatGPTContent.create({
-      list_id: `${listId}`,
-      sender: 'user',
-      content: content
-    });
-
-    // 이후 middleware/chatgpt 실행
-    const response = await callChatGPT(content);
-
-    // if ( response에 /n이 포함되어 있으면 ) -> 분리해서 넣기
-    if (response.content.includes('\n')) {
-      const list = response.content.split('\n');
-      console.log('list : ', list);
-      for (let i = 0; i < list.length; i++) {
-        if (list[i]) {
-          await models.ChatGPTContent.create({
-            list_id: `${listId}`,
-            sender: 'assistant',
-            content: list[i],
-            question_num: i + 1
-          });
-        }
+      if (prompt) {
+        content =
+          '다음은 자기소개서야:' +
+          `${prompt}` +
+          ' 이걸 읽고 ' +
+          `${type}` +
+          '면접 질문 ' +
+          `${count}` +
+          '개 해 줘';
+      } else {
+        content = `${type}` + '면접 질문 ' + `${count}` + '개 해 줘';
       }
-    } else {
+      console.log('content = ', content);
+      // 기존 메시지가 존재하지 않는다고 가정
       await models.ChatGPTContent.create({
         list_id: `${listId}`,
-        sender: 'assistant',
-        content: response.content
+        sender: 'user',
+        content: content
       });
-    }
 
-    // 결과를 API POST의 결과로 return
-    res.json('ok');
+      // 이후 middleware/chatgpt 실행
+      const response = await callChatGPT(content);
+
+      // if ( response에 /n이 포함되어 있으면 ) -> 분리해서 넣기
+      if (response.content.includes('\n')) {
+        const list = response.content.split('\n');
+        console.log('list : ', list);
+        for (let i = 0; i < list.length; i++) {
+          if (list[i]) {
+            await models.ChatGPTContent.create({
+              list_id: `${listId}`,
+              sender: 'assistant',
+              content: list[i],
+              question_num: i + 1
+            });
+          }
+        }
+      } else {
+        await models.ChatGPTContent.create({
+          list_id: `${listId}`,
+          sender: 'assistant',
+          content: response.content
+        });
+      }
+
+      // 결과를 API POST의 결과로 return
+      res.json('ok');
+    }
   } catch (err) {
     console.log(err);
   }
--- controller/chatGPT/postList.js
+++ controller/chatGPT/postList.js
@@ -7,13 +7,30 @@ const postList = async (req, res, next) => {
     const userId = Number(req.params.user_id);
     const listName = req.body.name;
 
-    const List = await models.ChatGPTList.create({
-      user_id: `${userId}`, // Foreign Key
-      name: listName
+    // 대화목록 개수 계산하기
+    const count = await models.ChatGPTList.findAndCountAll({
+      where: {
+        user_id: userId
+      }
     });
 
-    // 결과를 API POST의 결과로 return
-    res.json(List);
+    if (count.count >= 30) {
+      res.status(400).send('대화목록을 더 이상 만들 수 없습니다.');
+    } else {
+      const duplication = await models.ChatGPTList.findAll({
+        where: { user_id: userId }
+      });
+      if (!duplication) {
+        const List = await models.ChatGPTList.create({
+          user_id: `${userId}`, // Foreign Key
+          name: listName
+        });
+        // 결과를 API POST의 결과로 return
+        res.json(List);
+      } else {
+        res.status(400).send('대화목록 이름이 이미 있습니다.');
+      }
+    }
   } catch (err) {
     console.log(err);
   }
