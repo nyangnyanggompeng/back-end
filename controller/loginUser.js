fad23ae72e3a213668db12428fa65e5353abdebe
home0712 <k07m12j@gmail.com>
2023-06-29 19:23:01 +0900

    [FEAT] 로그인 api 구현

---
+++ controller/loginUser.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import db from '../models/index.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+function showUsers (req, res) {
+    
+}
+
+async function login (req, res) {
+    const username = req.body.username;
+    const domain = req.body.domain;
+    const password = req.body.password;
+
+    if (username && domain && password) {
+        // EMAIL CHECK
+        const users = await db.User.findAll({
+            where: {username: username, domain: domain}
+        });
+
+        if (users === null) { // EMAIL DOESN'T EXISTS
+            res.status(400).send('입력한 이메일 존재하지 않음');
+        } else { // EMAIL EXIST
+            let check = await bcrypt.compare(password, users[0].password);
+            if (!check) { // PASSWORD CHECK FALSE (LOGIN FAILURE)
+                res.status(400).send('비밀번호 틀림');
+            } else { // PASSWORD CHECK TRUE (LOGIN SUCCESS)
+                const accessToken = jwt.sign({id: users[0].id, isAdmin: users[0].isAdmin}, process.env.ACCESS_TOKEN_SECRET_KEY, {expiresIn: '60m'});
+                res.cookie('accessToken', accessToken, {httpOnly: true});
+                res.status(200).send('토큰 전송 완료');
+            }
+        }
+    } else {
+        res.status(400).send('이메일 혹은 비밀번호가 입력되지 않음');
+    }
+}
+
+export default {
+    showUsers,
+    login
+}
---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+function auth(req, res, next) {
+    try { // 인증 성공
+        console.log(req.headers.authorization.split('Bearer ')[1]);
+        req.decoded = jwt.verify(req.headers.authorization.split('Bearer ')[1], process.env.ACCESS_TOKEN_SECRET_KEY);
+        return next();
+    } catch (err) { // 인증 실패
+        if (err.name === 'TokenExpiredError') { // 유효시간 초과된 경우
+            return res.status(419).send("토큰 만료");
+        }
+        if (err.name === 'JsonWebTokenError') { // 토큰 비밀키 일치하지 않는 경우
+            return res.status(401).send("유효하지 않은 토큰");
+        }
+    }
+}
+
+export default {
+    auth
+}
